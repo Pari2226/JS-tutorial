ff41f6e674b5d9d1d0913639c7b2d875cd394a1c
Pari. <142119128+Pari2226@users.noreply.github.com>
2024-09-10 20:30:47 +0000

    container config done

---
+++ basics/conver.js
@@ -0,0 +1,17 @@
+let score = "33abc" 
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+let isLoggedIn = "" // false
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+let someNumber = 33
+
+let stringNumber = String(someNumber) // conversion
+console.log(typeof stringNumber);
