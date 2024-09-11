1e28e2bf8eef80f8d3f4035d934654868d143585
Pari. <142119128+Pari2226@users.noreply.github.com>
2024-09-11 20:19:28 +0000

    Container config done

--- basics/conver.js
+++ basics/conver.js
@@ -15,3 +15,34 @@ let someNumber = 33
 
 let stringNumber = String(someNumber) // conversion
 console.log(typeof stringNumber);
+
+// ******Operations******
+
+let value = 3;
+let negValue = -value
+//console.log(negValue);
+
+console.log(2**2);
+console.log(2%3);
+
+let str1 = "hello"
+let str2 = " heelooo"
+
+let str3 = str1 + str2;
+console.log(str3);
+
+// console.log("1" + 2); 
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log((3+4) * 5 % 3);
+// +true is possible
+// console.log(+true);
+
+let num1, num2 , num3;
+
+num1 = num2 = num3 = 2+2 
+
+let gameCounter = 100;
+//gameCounter++;
+++gameCounter;
+console.log(gameCounter);
