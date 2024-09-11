f5a8de24cf7b5be18f2c9cfbf7b7059366b277d8
Pari. <142119128+Pari2226@users.noreply.github.com>
2024-09-11 20:52:21 +0000

    Container config done

---
+++ basics/data.js
@@ -0,0 +1,30 @@
+// * Primitive 
+
+// 7 types: String , Number, Boolean, NULL, undefined , symbol, BigInt
+const scoreValue = 100.3
+
+const outsideTemp = null
+let userEmail;
+const id = Symbol('123')
+const idd = Symbol('123')
+console.log(id === idd)
+
+
+// const bigNumber = 16527534501324n // BIG INT
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const hero = ["shakiman", "naagraj", "doga"]
+let myObj={
+    name: "bella",
+    age: 22,
+}
+
+const Functions = function(){
+    console.log("hello world")
+}
+
+console.log(typeof bigNumber);
+console.log(typeof Functions);
+
