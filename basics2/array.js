2bc99e2053039026fdd76ee45d9cf4cce8be33a8
Pari. <142119128+Pari2226@users.noreply.github.com>
2024-09-12 20:49:35 +0000

    Container config done

---
+++ basics2/array.js
@@ -0,0 +1,13 @@
+//arrays
+const myArr = [0,23,46,6,7,8]
+const myHero = ["shasha"]
+console.log(myArr[0]);
+myArr.push(6)
+myArr.push(7)
+myArr.pop()
+
+myArr.unshift(9)
+console.log(myHero);
+console.log(myArr);
+
+
